5cb3bb46cc2e582374902a25c6af3300fd712324
Tyler S Breese <tylersbreese@Tylers-MacBook-Pro.local>
2021-10-18 15:18:06 -0400

    did more work on euchre.cpp

--- euchre.cpp
+++ euchre.cpp
@@ -67,9 +67,7 @@ int main(int argc, char *argv[])
     //create players
     std::vector<Player *> pArray;
     for (size_t i = 0; i < 4; i++) {
-        std::string name = argv[5+(2*i)];
-        std::string type = argv[6+(2*i)];
-        pArray.push_back(Player_factory(name, type));
+        pArray.push_back(Player_factory(argv[4+(2*i)], argv[5+(2*i)]));
     }
 
     //print first line
@@ -98,11 +96,51 @@ int main(int argc, char *argv[])
         int dealer = hand % 4;
         std::cout << pArray[dealer]->get_name() << " deals";
 
-        //deal cards
+        //set variables to deal cards
         int dealPattern[] = {3,2,3,2,2,3,2,3};
-        for (size_t i = 0; i < 2; i++) {
+        int dealPatternI = 0;
 
+        //loop through players starting with player to the left of dealer
+        for (size_t i = dealer+1; i < dealer+9; i++) {
+
+            //get player number
+            int playerNum = i % 4;
+
+            //deal specific number of cards based on the deal pattern
+            for (int c = 0; c < dealPattern[dealPatternI]; c++) {
+                pArray[playerNum]->add_card(p.deal_one());
+            }
+
+            //increment deal pattern
+            dealPatternI++;
         }
+
+        //deal upcard
+        Card up_card = p.deal_one();
+
+        //print upcard
+        std::cout << up_card.get_rank() << " of " << up_card.get_suit() << " turned up" << std::endl;
+        
+        //loop through players to establish trump
+        int count = 0;
+        bool trump_ordered = false;
+        int round_order = 0;
+        int player_ordered = 0;
+        std::string order_up_suit = " ";
+        //loop through players starting with player to the left of dealer
+        for (size_t i = dealer+1; i < dealer+9; i++) {
+            //get player number
+            int playerNum = i % 4;
+
+            //offer players chance to make upcard during first round
+            if (count < 4) {
+                trump_ordered = pArray[playerNum]->make_trump(up_card,playerNum==dealer,1,order_up_suit);
+                if (trump_ordered) {
+                    
+                }
+            }  
+        }
+
 
     }
 
